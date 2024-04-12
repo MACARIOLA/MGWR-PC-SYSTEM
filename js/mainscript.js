bef9a3558619ad421cb0027ff33ca2b31f886d0e
Joshua Alen Lopez <118281707+Joshieepooo@users.noreply.github.com>
2024-04-12 14:33:56 +0800

    Update - V0.1
    Added codes for the Feedbacks Page

--- js/mainscript.js
+++ js/mainscript.js
@@ -36,3 +36,71 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+
+
+$('.testimonials-container').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:12000,
+    margin:10,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:1,
+            nav:true
+        },
+        768:{
+            items:2
+        },
+    }
+})
+
+
+
+const allStar = document.querySelectorAll('.rating .star')
+const ratingValue = document.querySelector('.rating input')
+
+allStar.forEach((item, idx)=> {
+	item.addEventListener('click', function () {
+		let click = 0
+		ratingValue.value = idx + 1
+
+		allStar.forEach(i=> {
+			i.classList.replace('bxs-star', 'bx-star')
+			i.classList.remove('active')
+		})
+		for(let i=0; i<allStar.length; i++) {
+			if(i <= idx) {
+				allStar[i].classList.replace('bx-star', 'bxs-star')
+				allStar[i].classList.add('active')
+			} else {
+				allStar[i].style.setProperty('--i', click)
+				click++
+			}
+		}
+	})
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const leaveFeedbackButton = document.getElementById('leaveFeedbackBtn');
+    const wrapper = document.querySelector('.wrapper');
+    const overlay = document.querySelector('.overlay');
+    const cancelButton = document.querySelector('.wrapper .cancel');
+
+    leaveFeedbackButton.addEventListener('click', function() {
+        wrapper.classList.add('show');
+        overlay.classList.add('show');
+    });
+
+    cancelButton.addEventListener('click', function() {
+        wrapper.classList.remove('show');
+        overlay.classList.remove('show');
+    });
+});
